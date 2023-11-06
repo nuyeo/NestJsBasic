6ee30257a6b5f610784ea490e2c6aa47ab3bfa58
yeonod <bluelayzxx@gmail.com>
2023-11-07 04:04:56 +0900

    feat : BoardService 생성

--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { BoardsController } from './boards.controller';
+import { BoardsService } from './boards.service';
 
-@Module({})
+@Module({
+  controllers: [BoardsController],
+  providers: [BoardsService],
+})
 export class BoardsModule {}
