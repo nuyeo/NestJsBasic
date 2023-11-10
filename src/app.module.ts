4fe5d20fa7cd92b0773cef210996e05e8c31dd3c
yeonod <bluelayzxx@gmail.com>
2023-11-10 16:10:52 +0900

    feat: root 모듈에 typeorm 모듈 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { BoardsModule } from './boards/boards.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeORMConfig } from './configs/typeorm.config';
 
 @Module({
-  imports: [BoardsModule],
+  imports: [TypeOrmModule.forRoot(typeORMConfig), BoardsModule],
 })
 export class AppModule {}
