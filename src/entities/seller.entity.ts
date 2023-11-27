cf344329330e182cbfcd1b730edb3a0ac94b6fc7
yeonod <bluelayzxx@gmail.com>
2023-11-27 18:48:22 +0900

    fix: seller.entity.ts column명 수정
    스네이크 케이스에서 카멜 케이스로 수정

--- src/entities/seller.entity.ts
+++ src/entities/seller.entity.ts
@@ -1,20 +1,20 @@
-import { Entity, Column } from "typeorm";
-import { CommonEntity } from "./common/common.entity";
+import { Entity, Column } from 'typeorm';
+import { CommonEntity } from './common/common.entity';
 
 @Entity({ name: 'seller' })
 export class SellerEntity extends CommonEntity {
-    @Column({ type: 'varchar', length: 32 })
-    name!: string;
+  @Column({ type: 'varchar', length: 32 })
+  name!: string;
 
-    @Column({ type: 'varchar', length: 512 })
-    hashedPassword!: string;
+  @Column({ type: 'varchar', length: 512 })
+  hashedPassword!: string;
 
-    @Column({ type: 'varchar', length: 128, unique: true })
-    email!: string;
+  @Column({ type: 'varchar', length: 128, unique: true })
+  email!: string;
 
-    @Column({ type: 'varchar', length: 16 })
-    phone!: string;
+  @Column({ type: 'varchar', length: 16 })
+  phone!: string;
 
-    @Column({ type: 'varchar', length: 32 })
-    business_number!: string;
+  @Column({ type: 'varchar', length: 32 })
+  businessNumber!: string;
 }
