4e7ba6fca2373f10afc7b186fece94fde26bf68c
yeonod <bluelayzxx@gmail.com>
2023-11-28 12:03:17 +0900

    fix: order 엔티티 칼럼 이름 수정
    유저 id 외래키 값인 id2를 직관적이도록 userId로 수정

--- src/entities/order.entity.ts
+++ src/entities/order.entity.ts
@@ -4,5 +4,5 @@ import { CommonEntity } from './common/common.entity';
 @Entity()
 export class OrderEntity extends CommonEntity {
   @Column({ type: 'bigint', length: 20 })
-  id2!: number;
+  userId!: number;
 }
