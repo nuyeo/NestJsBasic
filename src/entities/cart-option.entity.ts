f551dc74ac36061cb7479b9e352b1ce6410b8ad0
yeonod <bluelayzxx@gmail.com>
2023-11-28 22:28:30 +0900

    fix: cart-option의 count 칼럼 삭제

--- src/entities/cart-option.entity.ts
+++ src/entities/cart-option.entity.ts
@@ -3,9 +3,6 @@ import { CommonEntity } from "./common/common.entity";
 
 @Entity()
 export class CartOptionEntity extends CommonEntity {
-    @Column({ type: 'int', length: 10 })
-    count!: string;
-
     @Column({ type: 'int' })
     cartId!: number;
 
