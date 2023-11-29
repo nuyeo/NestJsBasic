2631dc767ea4442359e1446201d80cf7627c2a86
yeonod <bluelayzxx@gmail.com>
2023-11-30 00:47:59 +0900

    feat: auth.service에 signUp 추가
    AuthCredentialsDto를 이용한 비동기 signUp 추가

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserRepository } from 'src/repositories/user.repository';
+import { AuthCredentialsDto } from './dto/auth-credential.dto';
 
 @Injectable()
 export class AuthService {
@@ -8,4 +9,8 @@ export class AuthService {
     @InjectRepository(UserRepository)
     private userRepository: UserRepository,
   ) {}
+
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    return this.userRepository.createUser(authCredentialsDto);
+  }
 }
