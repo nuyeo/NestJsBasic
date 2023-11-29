0db36f564eb36d3f2418cacf909e5c5151c74ecc
yeonod <bluelayzxx@gmail.com>
2023-11-30 00:44:37 +0900

    feat: user.repository에 createUser 추가
    비동기로 AuthCredentialsDto를 이용해 유저를 생성하는 createUser 기능
    추가

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,6 +1,14 @@
+import { AuthCredentialsDto } from 'src/auth/dto/auth-credential.dto';
 import { CustomRepository } from 'src/configs/custom-typeorm.decorator';
 import { UserEntity } from 'src/entities/user.entity';
 import { Repository } from 'typeorm';
 
 @CustomRepository(UserEntity)
-export class UserRepository extends Repository<UserEntity> {}
+export class UserRepository extends Repository<UserEntity> {
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { email, hashedPassword } = authCredentialsDto;
+    const user = this.create({ email, hashedPassword });
+
+    await this.save(user);
+  }
+}
