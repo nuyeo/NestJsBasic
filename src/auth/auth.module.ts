3bcb752c2702a9b4c9b66e299f74cc68d3e54205
yeonod <bluelayzxx@gmail.com>
2023-11-30 00:22:14 +0900

    feat: auth.module에 userRepository import 추가
    user.repository를 다른 곳에서 사용하기 위해 추가

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserRepository } from 'src/repositories/user.repository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UserRepository])],
   controllers: [AuthController],
   providers: [AuthService],
 })
