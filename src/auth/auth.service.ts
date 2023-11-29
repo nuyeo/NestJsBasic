94cc2b59d894b5e49b3e3b4682ebbb05b9162963
yeonod <bluelayzxx@gmail.com>
2023-11-30 00:24:48 +0900

    feat: auth.service에 userRepository 추가
    userRepository를 auth Service 안에서 사용하기 위해 유저 리포지토리 추가

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserRepository } from 'src/repositories/user.repository';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(UserRepository)
+    private userRepository: UserRepository,
+  ) {}
+}
