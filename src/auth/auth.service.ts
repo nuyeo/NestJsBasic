9a3e7fc2dcf748f6184bd13b1174263e1d5c3d8d
yeonod <bluelayzxx@gmail.com>
2023-12-01 16:03:04 +0900

    feat: auth.service에 비밀번호 일치 여부를 판단하는 signIn 추가
    비동기 signIn으로 입력된 비밀번호가 입력된 email에 해당하는 비밀번호
    값인지 bcrypt로 저장된 hashedPassword와 비교해서 일치하지 않을 경우
    UnauthorizedException 예외 처리

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserRepository } from 'src/repositories/user.repository';
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
@@ -12,5 +13,16 @@ export class AuthService {
 
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return this.userRepository.createUser(authCredentialsDto);
+  }
+
+  async signIn(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+    const { email, password } = authCredentialsDto;
+    const user = await this.userRepository.findOne({ email });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      return 'login success';
+    } else {
+      throw new UnauthorizedException('login failed');
+    }
   }
 }
